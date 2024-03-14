072fb59028a168aa4db6f0754c56057144018675
divyansh.gupta <divyansh.gupta@crmnext.com>
2024-03-14 23:47:08 +0530

    Add category screen and add packages on yaml

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,417 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:query_app/constants/measurement_repo.dart';
+import 'package:query_app/databaseManager/my_shared_preferences.dart';
+import 'package:query_app/keys/key_showcase.dart';
+import 'package:query_app/modal/color_modal.dart';
+import 'package:query_app/screens/login_screen.dart';
+import 'package:query_app/screens/question_builder.dart';
+import 'package:query_app/screens/widget/custom_drawer.dart';
+import 'package:query_app/screens/widget/loading_view.dart';
+import 'package:query_app/screens/widget/notification_listen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:showcaseview/showcaseview.dart';
+import '../databaseManager/firebase_repo.dart';
+import '../main.dart';
+import '../modal/paint_modal.dart';
+import '../route/animated_route.dart';
+
+class CategoryScreen extends StatefulWidget {
+  const CategoryScreen({Key? key}) : super(key: key);
+
+  static String id = '/category';
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  TextEditingController query = TextEditingController();
+  final List<IconData> icons = [
+    Icons.developer_mode,
+    Icons.mobile_friendly_sharp,
+    Icons.code,
+    Icons.developer_board,
+  ];
+
+  List categoryList = [];
+  final colorModal = MyColorsModal();
+  String tokenSender = '';
+
+  FireBaseDataBaseRepo fireBaseDB = FireBaseDataBaseRepo();
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> myCategoryData = [];
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> myQAData = [];
+  late SharedPreferences sharedPreferences;
+  bool isLogin = false;
+  bool isDrawer = false;
+  String? profileName;
+
+  // ShowCaseKeys showCaseKeys = ShowCaseKeys();
+
+  @override
+  void initState() {
+    fetchDatabaseList();
+    super.initState();
+    getProfileName();
+    MyNotificationListen().getNotificationListenData(
+        navigatorKey.currentState?.context ?? context);
+  }
+
+  fetchDatabaseList() async {
+    isLogin = (await MySharedPreferences.getUserDefault('userLogin'))!;
+    myCategoryData = await fireBaseDB.getDocsList('category');
+    //print('mySnap collection FirebaseFireStore : $myCategoryData');
+    //print('mySnap collection FirebaseFireStore : ${myCategoryData[0].data()}');
+  }
+
+  Future<void> getProfileName() async {
+    profileName =
+        await MySharedPreferences.getStringUserDefault('profileName') ??
+            "Guest";
+  }
+
+  void onPressedDrawerCloseIcon() => setState(() => isDrawer = false);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Colors.green,
+      body: ShowCaseWidget(
+        onStart: (index, key) {
+          // print('onStart: $index, $key , ${key.currentContext}');
+        },
+        autoPlayDelay: const Duration(seconds: 3),
+        onComplete: (index, key) {
+          // print('onComplete: $index, $key');
+          if (index == 5) {
+            SystemChrome.setSystemUIOverlayStyle(
+              SystemUiOverlayStyle.light.copyWith(
+                statusBarIconBrightness: Brightness.dark,
+                statusBarColor: Colors.white,
+              ),
+            );
+          }
+        },
+        blurValue: 1,
+        builder: Builder(
+          // key: ShowCaseKeys.contextKey,
+          builder: (context) {
+            return Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: MeasurementRepo.edgeTopFiftyOtherTwenty,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Showcase(
+                            key: ShowCaseKeys.two,
+                            description: 'Click to open drawer',
+                            disableDefaultTargetGestures: true,
+                            onBarrierClick: () {
+                              debugPrint("on barrier click .");
+                            },
+                            child: IconButton(
+                              icon:
+                                  const Icon(CupertinoIcons.line_horizontal_3),
+                              onPressed: () {
+                                isDrawer = !isDrawer;
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: AnimatedTextKit(
+                              repeatForever: true,
+                              animatedTexts: [
+                                ColorizeAnimatedText(
+                                  'Categories',
+                                  textStyle: GoogleFonts.getFont('Roboto Slab',
+                                      fontSize: 40.0,
+                                      fontWeight: FontWeight.bold),
+                                  colors: [
+                                    Colors.black,
+                                    Colors.indigoAccent,
+                                    Colors.pinkAccent,
+                                    Colors.orangeAccent
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          // FutureBuilder(
+                          //   future: MySharedPreferences.getUserDefault(
+                          //       'userLogin'),
+                          //   builder: (context, snapshot) {
+                          //     isLogin = snapshot.data ?? false;
+                          //     if (!isLogin) {
+                          //       return IconButton(
+                          //         onPressed: () async {
+                          //           Navigator.push(
+                          //             context,
+                          //             MaterialPageRoute(
+                          //               builder: (context) =>
+                          //                   const LoginPage(),
+                          //             ),
+                          //           );
+                          //           await MySharedPreferences.setUserDefault(
+                          //               'userLogin', true);
+                          //           //print('pref.isLogin : $isLogin');
+                          //         },
+                          //         icon: const Icon(
+                          //           Icons.login,
+                          //           size: 20,
+                          //         ),
+                          //       );
+                          //     }
+                          //     return IconButton(
+                          //       onPressed: () async {
+                          //         Navigator.push(
+                          //           context,
+                          //           MaterialPageRoute(
+                          //             builder: (context) => const LoginPage(),
+                          //           ),
+                          //         );
+                          //         await MySharedPreferences.setUserDefault(
+                          //             'userLogin', false);
+                          //         //print('pref.isLogin : $isLogin');
+                          //       },
+                          //       icon: const Icon(
+                          //         Icons.logout,
+                          //         size: 20,
+                          //       ),
+                          //     );
+                          //   },
+                          // ),
+                        ],
+                      ),
+                    ),
+                    FutureBuilder(
+                      future: fireBaseDB.getDocsList('category'),
+                      builder: (context, snapshot) {
+                        myCategoryData = snapshot.data ?? [];
+                        if (snapshot.hasError) {
+                          return LoadingDataView(
+                              text: snapshot.error.toString());
+                        }
+
+                        if (myCategoryData.isEmpty) {
+                          return const LoadingDataView(
+                            text: 'Please Wait',
+                          );
+                        }
+                        return Expanded(
+                          child: GridView.count(
+                            childAspectRatio: 1,
+                            crossAxisCount: 2,
+                            children: List.generate(
+                              myCategoryData.length,
+                              (index) {
+                                final titleId = myCategoryData[index].id;
+
+                                Widget setCategoryWidget() {
+                                  return InkWell(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        AnimatedRoute(
+                                          route: QuestionBank(
+                                            categoryID: titleId,
+                                            title: myCategoryData[index]
+                                                ['title'],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    child: AnimatedContainer(
+                                      margin: MeasurementRepo.edgeAllTen,
+                                      padding: MeasurementRepo.edgeAllTen,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            width: 0.08, color: Colors.black45),
+                                        borderRadius: BorderRadius.circular(20),
+
+                                        gradient: MyColorsModal
+                                            .cardGradientColors()[index % 6],
+                                        // boxShadow: [
+                                        //   BoxShadow(
+                                        //       color: Colors.grey,
+                                        //       spreadRadius: 0,
+                                        //       blurRadius: 6,
+                                        //       offset: Offset(0,2)
+                                        //   ),
+                                        // ],
+                                      ),
+                                      duration: const Duration(seconds: 1),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.stretch,
+                                        children: [
+                                          Text(
+                                            myCategoryData[index]['title'],
+                                            textAlign: TextAlign.center,
+                                            style: GoogleFonts.getFont(
+                                                'Roboto Slab',
+                                                fontSize: 18.0,
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Expanded(
+                                            child: Image(
+                                              // width:
+                                              //     MediaQuery.of(context).size.width *
+                                              //         0.8,
+                                              // height: MediaQuery.of(context)
+                                              //         .size
+                                              //         .height *
+                                              //     0.2, //animationController.value,
+                                              // image: AssetImage(
+                                              //     'assets/images/cat_${index + 1}.png'),
+                                              image: NetworkImage(
+                                                myCategoryData[index]['image'],
+                                              ),
+                                              fit: BoxFit.contain,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }
+
+                                return (ShowCaseKeys.isShowcase && index == 0)
+                                    ? Showcase(
+                                        key: ShowCaseKeys.four,
+                                        description:
+                                            'Tap to navigate to queries related to that category.',
+                                        onTargetClick: () {
+                                          // print("target click object");
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  QuestionBank(
+                                                categoryID: titleId,
+                                                title: myCategoryData[0]
+                                                    ["title"],
+                                              ),
+                                            ),
+                                          ).then((_) {
+                                            ShowCaseWidget.of(context).next();
+                                          });
+
+                                          // print("on target complete : $titleId , ${myCategoryData[0]["title"]}");
+                                        },
+                                        disposeOnTap: true,
+                                        child: setCategoryWidget(),
+                                      )
+                                    : setCategoryWidget();
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Stack(
+                      children: [
+                        CustomPaint(
+                            painter: CustomPainterClass(),
+                            size: Size(MediaQuery.of(context).size.width, 80)),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 80,
+                          child: ListView.separated(
+                            padding: MeasurementRepo.edgeAllTen,
+                            scrollDirection: Axis.horizontal,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: icons.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  if (myCategoryData.isNotEmpty) {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => QuestionBank(
+                                          title: myCategoryData[index]["title"],
+                                          categoryID: myCategoryData[index].id,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                  // else {
+                                  //   print('Navigation disable due to empty data from firebase.');
+                                  // }
+                                },
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.white,
+                                      radius: 20,
+                                      child: Icon(
+                                        icons[index],
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    if (index == 1)
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.23,
+                                      ),
+                                  ],
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) => SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.1,
+                            ),
+                          ),
+                        ),
+                        Center(
+                          heightFactor: 0.8,
+                          child: FloatingActionButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                AnimatedRoute(
+                                  route: const LoginPage(),
+                                ),
+                              );
+                            },
+                            backgroundColor: MyColorsModal.kThemeColor,
+                            elevation: 0.1,
+                            child: const Icon(
+                              Icons.home_rounded,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                (isDrawer)
+                    ? CustomDrawerView(
+                        onPressedCloseIcon: onPressedDrawerCloseIcon,
+                        profileName: profileName!,
+                      )
+                    : const Center(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
