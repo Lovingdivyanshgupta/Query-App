f140badde9ea0702ffe9f739e767ca295ba3015d
divyansh.gupta <divyansh.gupta@crmnext.com>
2024-05-11 22:52:22 +0530

    compileSdkVersion 34 , add no record image and modify loading view

--- lib/custom/widgets/loading_view.dart
+++ lib/custom/widgets/loading_view.dart
@@ -5,23 +5,26 @@ import '../../constant/measurement_repo.dart';
 import '../../modal/color_modal.dart';
 
 class LoadingDataView extends StatelessWidget {
-  const LoadingDataView({Key? key, required this.text}) : super(key: key);
+  const LoadingDataView({Key? key, required this.text, this.isShowLoader}) : super(key: key);
 
   final String text;
+  final bool? isShowLoader;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
+          (isShowLoader ?? false) ? Padding(
             padding: MeasurementRepo.edgeAllFifteen,
             child: const Center(
               child: CircularProgressIndicator(
                 color: MyColorsModal.kThemeColor,
               ),
             ),
-          ),
+          ) : const SizedBox(),
+          Image.asset("assets/images/no_record.png",width: 200,height: 200,),
           Text(
             text,
             style: GoogleFonts.getFont(
